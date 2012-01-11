81481c93e921350b8b319fdcd9cd655bb5d9c744
sinnus <sinnus@sinnuss-Mac-Pro.local>
2012-01-11 22:23:38 +0300

    Initial Commit

---
+++ CoreDataUtil/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoreDataUtil
+//
+//  Created by A B on 1/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
